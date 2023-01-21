0a71cfce77af29d8fbb325186b40755f68bb8700
IcyFoxe <48529386+IcyFoxe@users.noreply.github.com>
2023-01-21 22:09:20 +0100

    Updated year of Vocabie

--- client/legacy/dexie-export-import.b99733df.js
+++ client/legacy/dexie-export-import.b99733df.js
@@ -1,4 +1,4 @@
-import{A as e}from"./client.b6653dd5.js";import{D as t}from"./database.acb5a169.js";
+import{A as e}from"./client.210233fe.js";import{D as t}from"./database.b76186e9.js";
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use
